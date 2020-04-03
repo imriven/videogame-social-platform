c4606c97e6e007b99339d77cd895da3f1b8f4ee8
Christina Harris <christinathedesigner@outlook.com>
2020-04-03 13:31:54 -0700

    the profile form might be different from seeing the actual profile. This is the form somebody will fill out when they first sign up

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,7 +7,14 @@ const Profile = () => {
     return (
     <div>
         <h3>This is the Profile Page</h3>
+      <div>
+        <p>Enter current games you are playing (Enter up to three)</p>
+      </div>
+
+
     </div>
+
+
   );
 }
 
